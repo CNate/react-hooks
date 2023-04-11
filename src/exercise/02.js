6b4d3edec4aef063334c52dcb9ab543c39a1a442
Nathan Velliquette <nathan.velliquette@tutanota.com>
2023-04-11 19:58:57 -0400

    exercise 2: EC4

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,14 +4,30 @@
 import * as React from 'react'
 import {useEffect} from 'react'
 
-const useLocalStorageState = (key, defaultValue = '') => {
-  const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) ?? defaultValue,
-  )
+const useLocalStorageState = (
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) => {
+  const [state, setState] = React.useState(() => {
+    const valueInStorage = window.localStorage.getItem(key)
+    if (valueInStorage) {
+      return deserialize(valueInStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
 
   useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state])
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+      window.localStorage.setItem(key, serialize(state))
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, state, serialize])
 
   return [state, setState]
 }
